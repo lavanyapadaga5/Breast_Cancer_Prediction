bebefd2624ac6aa3217f3b2824ccf9009f3d60eb
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-01-05 20:28:02 +0530

    Update README.py

--- README.py
+++ README.py
@@ -6,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 
 # loading the saved model
-breast_cancer_model = pickle.load(open('breast_cancer_model.sav', 'rb'))
+breast_cancer_model = pickle.load(open('breast_cancer_Pred.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',
+    selected = option_menu('Disease Prediction System',
                            ['Breast Cancer Prediction'],
                            icons=['person'],
                            default_index=0)
